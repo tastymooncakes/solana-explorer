9255e7ff8ee795539f507926e739305abbaa011f
Ethan Wu <ethanwu@Ethans-MacBook-Air.local>
2025-05-01 20:17:50 +0800

    Add edge runtime configuration to dynamic routes

--- src/app/block/[slot]/page.tsx
+++ src/app/block/[slot]/page.tsx
@@ -1,5 +1,7 @@
 import BlockDetailPage from "@/components/blocks/BlockDetailPage";
 
+export const runtime = 'edge';
+
 export default function Page() {
     return (
         <BlockDetailPage />
--- src/app/tx/[signature]/page.tsx
+++ src/app/tx/[signature]/page.tsx
@@ -1,5 +1,7 @@
 import TransactionDetailPage from "@/components/transactions/TransactionDetailPage";
 
+export const runtime = 'edge';
+
 export default function Page() {
     return (
         <TransactionDetailPage />
